a19aa6711e25d736777bb6a027a4ada6747067ad
lncendia <egor.lazeba@yandex.ru>
2021-02-08 22:42:22 +0300

    New token.

--- Insta/Program.cs
+++ Insta/Program.cs
@@ -4,7 +4,7 @@ namespace Insta
 {
     internal static class Program
     {
-        public const string Token = "1485092461:AAGcPpPwxfSTnQ8cM3FWPFirvGIDjs84Pto";//"1682222171:AAGw4CBCJ875NRn1rFnh0sBncYkev5KIa4o";
+        public const string Token = "1682222171:AAGw4CBCJ875NRn1rFnh0sBncYkev5KIa4o";
 
         static void Main(string[] args)
         {
