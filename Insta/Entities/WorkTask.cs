d15056fc5a1370461ebeb286ed0bf557536ed378
lncendia <egor.lazeba@yandex.ru>
2021-02-20 17:06:05 +0300

    Saving tasks in db

---
+++ Insta/Entities/WorkTask.cs
@@ -0,0 +1,16 @@
+﻿using Insta.Working;
+using System;
+
+namespace Insta.Entities
+{
+    public class WorkTask
+    {
+        public int Id { get; set; }
+        public Instagram Instagram { get; set; }
+        public string Hashtag { get; set; }
+        public int LowerDelay { get; set; }
+        public int UpperDelay { get; set; }
+        public Work.Mode Mode { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
